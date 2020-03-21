c93a36c5fb147c7a36820900c7f25084be430503
Carlos Alcantara <alcantaracarlosmiguelr@gmail.com>
2020-03-22 05:34:33 +0800

    added basic counter to each message

--- messenger.py
+++ messenger.py
@@ -4,6 +4,18 @@ from _deploymentv2.inference import inference
 import os
 import time
 
+max_msg_per_min = 5
+max_msg_per_hour = 50
+second_level_counter = 0
+reply_counter = 0
+
+def get_message(message_text):
+    #gets the neural net output and extracts only the text
+    bot_output = inference(message_text)
+    bot_message = bot_output['answers']
+    bot_message = bot_message[0]
+    return bot_message
+
 class CarlosBot(Client):
     #def get_and_send_message(message_text, thread_id, thread_type):
 
@@ -12,13 +24,15 @@ class CarlosBot(Client):
         #self.markAsRead(thread_id)
         global second_level_counter
         global reply_counter
+        global max_msg_per_min
+        global max_msg_per_hour
 
         #Limits to 3 messages per minute with a max of 50 messages per hour
-        if second_level_counter > 49:
+        if second_level_counter > max_msg_per_hour - 1:
             time.sleep(60*60)
             second_level_counter = 0
 
-        if reply_counter > 4: 
+        if reply_counter > max_msg_per_min - 1: 
             time.sleep(60)
             reply_counter = 0
             
@@ -35,10 +49,10 @@ class CarlosBot(Client):
                     message_text = message_text.replace('@Carlosbot', '')
                     print(message_text)
                     #get_and_send_message(message_text, thread_id, thread_type)
-                    #gets the neural net output and extracts only the text
-                    bot_output = inference(message_text)
-                    bot_message = bot_output['answers']
-                    bot_message = bot_message[0]
+                    bot_message = get_message(message_text)
+
+                    #adds a counter for remaining messages left in the minute
+                    bot_message = ' '.join([bot_message,remaining_message])
 
                     #sends the message and prints output
                     self.send(Message(bot_message), thread_id=thread_id, thread_type=thread_type)
@@ -50,10 +64,12 @@ class CarlosBot(Client):
             #If it's a user, reply immediately
             elif thread_type == ThreadType.USER:
                 #get_and_send_message(message_text, thread_id, thread_type)
-                #gets the neural net output and extracts only the text
-                bot_output = inference(message_text)
-                bot_message = bot_output['answers']
-                bot_message = bot_message[0]
+                bot_message = get_message(message_text)
+
+                #adds a counter for remaining messages left in the minute
+                msgs_left = max_msg_per_min - reply_counter
+                remaining_message = '[Msgs Left: {}]'.format(msgs_left)
+                bot_message = ' '.join([bot_message,remaining_message])
 
                 #sends the message and prints output
                 self.send(Message(bot_message), thread_id=thread_id, thread_type=thread_type)
@@ -62,14 +78,9 @@ class CarlosBot(Client):
                 second_level_counter += 1
                 print('Counter: {}'.format(second_level_counter))
 
-second_level_counter = 0
-reply_counter = 0
 while True:
     try:
         client = CarlosBot("bobcarlos545@gmail.com", "bobocarlos505")
         client.listen()
-        client.logout()
     except:
         pass
-    else:
-        break
