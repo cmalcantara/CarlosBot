8083506b592465e4b81a5b22fc7db6f21b280cdb
Carlos Alcantara <alcantaracarlosmiguelr@gmail.com>
2020-03-23 16:53:10 +0800

    fixed issues with pending messages and refactored global variables

--- messenger.py
+++ messenger.py
@@ -4,20 +4,22 @@ from _deploymentv2.inference import inference
 import os
 import time
 
-introduction = 'Hi, I\'m Carlosbot, a neural network chatbot by Carlos A. if you want to chat, just @ me or pm directly. Though, I am still quite dumb and will be improved later on.'
-max_msg_per_min = 15
-max_msg_per_hour = 50
-second_level_counter = 0
+group_introduction = 'Hi, I\'m Carlosbot, a neural network chatbot by Carlos A. if you want to chat, just @ me or pm directly. Though, I am still quite du b and will be improved later on.'
+user_introduction = 'Hi, I\'m Carlosbot, a neural network chatbot by Carlos A. I\'m here if you want to chat. Though, I am still quite dumb and will be improved later on.'
+
+sleep_period = 20
+per_msg_delay = 1.5
+max_msg_per_30s = 15
 reply_counter = 0
 
-def output(self, message_text, max_msg_per_min, reply_counter, thread_id, thread_type): 
+def output(self, message_text, max_msg_per_30s, reply_counter, thread_id, thread_type): 
     bot_message = get_message(message_text)
-    #bot_message = add_a_counter(bot_message, max_msg_per_min, reply_counter)
+    #bot_message = add_a_counter(bot_message, max_msg_per_30s, reply_counter)
     send_message(self, bot_message, thread_id, thread_type)
 
-def add_a_counter(bot_message, max_msg_per_min, reply_counter):
+def add_a_counter(bot_message, max_msg_per_30s, reply_counter):
     #adds a counter for remaining messages left in the minute
-    msgs_left = max_msg_per_min - reply_counter - 1
+    msgs_left = max_msg_per_30s - reply_counter - 1
     remaining_message = '[Msgs left this min: {}]'.format(msgs_left)
     bot_message = ' '.join([bot_message,remaining_message])
     return bot_message
@@ -25,9 +27,10 @@ def add_a_counter(bot_message, max_msg_per_min, reply_counter):
 def send_message(self, bot_message, thread_id, thread_type):
     global second_level_counter
     global reply_counter
+    global per_msg_delay
     #sends the message and prints output
     self.send(Message(bot_message), thread_id=thread_id, thread_type=thread_type)
-    time.sleep(1.5)
+    time.sleep(per_msg_delay)
     print('Output: {}'.format(bot_message))
     reply_counter += 1
     second_level_counter += 1
@@ -42,24 +45,37 @@ def get_message(message_text):
 
 class CarlosBot(Client):
     #def get_and_send_message(message_text, thread_id, thread_type):
+    
+    def onPendingMessage(self, thread_id, thread_type, metadata, msg):
+        global user_introduction
+        """Called when the client is listening, and somebody that isn't
+         connected with you on either Facebook or Messenger sends a message.
+         After that, you need to use fetchThreadList to actually read the message.
+
+         Args:
+            thread_id: Thread ID that the message was sent to. See :ref:`intro_threads`
+            thread_type (ThreadType): Type of thread that the message was sent to. See :ref:`intro_threads`
+            metadata: Extra metadata about the message
+            msg: A full set of the data received
+        """
+        #log.info("New pending message from {}".format(thread_id))
+        #threads = self.fetchThreadList(thread_location=ThreadLocation.PENDING)
+        #for thread in threads:
+        self.send(Message(user_introduction), thread_id=thread_id, thread_type=thread_type)
+
 
     def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
         #self.markAsDelivered(thread_id, message_object.uid)
         #self.markAsRead(thread_id)
         global second_level_counter
         global reply_counter
-        global max_msg_per_min
-        global max_msg_per_hour
-        global introduction
+        global max_msg_per_30s
+        global group_introduction
+        global sleep_period
 
-        #Limits to 3 messages per minute with a max of 50 messages per hour
-        #if second_level_counter > max_msg_per_hour - 1:
-        #    time.sleep(60*60)
-        #    second_level_counter = 0
-
-        #acutally 30 sec
-        if reply_counter > max_msg_per_min - 1: 
-            time.sleep(20)
+        #Limits to max_msg_per_30sec
+        if reply_counter > max_msg_per_30s - 1: 
+            time.sleep(sleep_period)
             reply_counter = 0
             
         # If you're not the author, use the chatbot
@@ -67,13 +83,11 @@ class CarlosBot(Client):
             message_text = message_object.text
             print('Input: {}'.format(message_text))
 
-
-
             # If it's a group, find '@CarlosBot'
             if thread_type == ThreadType.GROUP:
                 if message_text.find("@Carlosbot") != -1:
                     if message_text.find('introduce yourself') != -1:
-                        self.send(Message(introduction), thread_id=thread_id, thread_type=thread_type)
+                        self.send(Message(group_introduction), thread_id=thread_id, thread_type=thread_type)
                     else:
                         #remove the '@CarlosBot Tensorflow'
                         message_text = message_text.replace('@Carlosbot Bob', '')
@@ -81,12 +95,12 @@ class CarlosBot(Client):
                         print(message_text)
 
                         #gets bot_message, adds a counter, sends the message
-                        output(self, message_text, max_msg_per_min, reply_counter, thread_id, thread_type)
+                        output(self, message_text, max_msg_per_30s, reply_counter, thread_id, thread_type)
 
             #If it's a user, reply immediately
             elif thread_type == ThreadType.USER:
                 #gets bot_message, adds a counter, sends the message
-                output(self, message_text, max_msg_per_min, reply_counter, thread_id, thread_type)
+                output(self, message_text, max_msg_per_30s, reply_counter, thread_id, thread_type)
 
 while True:
     client = CarlosBot("bobcarlos545@gmail.com", "bobocarlos505")
